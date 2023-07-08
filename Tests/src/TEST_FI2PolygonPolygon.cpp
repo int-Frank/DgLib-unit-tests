a38b45af74e0a3c864f14898963de4214ee39a54
Frank Hart <frankhart010@gmail.com>
2023-07-08 13:31:45 +1000

    Updated unit tests

--- Tests/src/TEST_FI2PolygonPolygon.cpp
+++ Tests/src/TEST_FI2PolygonPolygon.cpp
@@ -12,29 +12,6 @@ typedef Dg::Segment2<Real>          seg;
 typedef Dg::Polygon2<Real>          polygon;
 typedef Dg::FI2PolygonPolygon<Real> fiquery;
 
-typedef Dg::impl_FI2PolygonPolygon::Graph<Real> graph;
-typedef Dg::impl_FI2PolygonPolygon::Node<Real> node;
-typedef Dg::impl_FI2PolygonPolygon::GraphBuilder<Real> graphBuilder;
-
-void PrintGraph(graph const &graph)
-{
-  std::cout << "\n\nGRAPH";
-
-  for (size_t i = 0; i < graph.nodes.size(); i++)
-  {
-    node const *pNode = &(graph.nodes[i]);
-    std::cout << '\n' << i << ": (" << pNode->vertex.x() << ", " << pNode->vertex.y() << "); [";
-    for (size_t j = 0; j < pNode->neighbours.size(); j++)
-    {
-      std::cout << pNode->neighbours[j].id;
-      if (j + 1 != pNode->neighbours.size())
-        std::cout << ", ";
-    }
-    std::cout << "]";
-  }
-  std::cout << "\n\n";
-}
-
 TEST(Stack_FIPolygonPolygon, FIPP_Orientation)
 {
   bool good = true;
@@ -48,7 +25,7 @@ TEST(Stack_FIPolygonPolygon, FIPP_Orientation)
     float x = std::cos(angle);
     float y = std::sin(angle);
     vec b(x, y);
-    float val = Dg::impl_FI2PolygonPolygon::GetOrientation(b, a);
+    float val = Dg::GetOrientation(a, b);
     if (val > prev)
     {
       good = false;
@@ -96,11 +73,10 @@ TEST(Stack_FIPolygonPolygon, FIPP_Graph_OverlappingPoint_0)
   polyB.PushBack(vec(2.f, 2.f));
   polyB.PushBack(vec(1.f, 2.f));
 
-  graph graph;
-  graphBuilder builder;
-  auto code = builder.Execute(polyA, polyB, &graph);
+  fiquery query;
+  auto result = query(polyA, polyB);
 
-  CHECK(code == Dg::QueryCode::Intersecting);
+  CHECK(result.code == Dg::QueryCode::Intersecting);
 }
 
 TEST(Stack_FIPolygonPolygon, FIPP_Graph_OverlappingPoint_1)
@@ -119,11 +95,10 @@ TEST(Stack_FIPolygonPolygon, FIPP_Graph_OverlappingPoint_1)
   polyB.PushBack(vec(0.f, 1.f));
   polyB.PushBack(vec(1.5f, 1.5f));
 
-  graph graph;
-  graphBuilder builder;
-  auto code = builder.Execute(polyA, polyB, &graph);
+  fiquery query;
+  auto result = query(polyA, polyB);
 
-  CHECK(code == Dg::QueryCode::Intersecting);
+  CHECK(result.code == Dg::QueryCode::Intersecting);
 }
 
 TEST(Stack_FIPolygonPolygon, FIPP_Graph_OverlappingEdge_0)
@@ -141,9 +116,8 @@ TEST(Stack_FIPolygonPolygon, FIPP_Graph_OverlappingEdge_0)
   polyB.PushBack(vec(0.f, 2.f));
   polyB.PushBack(vec(0.f, 1.f));
 
-  graph graph;
-  graphBuilder builder;
-  auto code = builder.Execute(polyA, polyB, &graph);
+  fiquery query;
+  auto result = query(polyA, polyB);
 
-  CHECK(code == Dg::QueryCode::Intersecting);
+  CHECK(result.code == Dg::QueryCode::Intersecting);
 }
